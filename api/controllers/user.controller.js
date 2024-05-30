65a23508ee8060c38db3ceed4626adc3f61c8775
Dispenser254 <stevensonkibs55@gmail.com>
2024-05-30 12:06:33 +0300

    Create user update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,34 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (request, response) => {
   response.json({ message: "Api route is working!!" });
+};
+
+export const updateuser = async (request, response, next) => {
+  if (request.user.id !== request.params.id) {
+    return next(errorHandler(401, "You can only update your own account!"));
+  }
+  try {
+    if (request.body.password) {
+      request.body.password = bcryptjs.hashSync(request.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      request.params.id,
+      {
+        $set: {
+          username: request.body.username,
+          email: request.body.email,
+          password: request.body.password,
+          avatar: request.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    response.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
